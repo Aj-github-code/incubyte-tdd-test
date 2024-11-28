8f9f8eadf846ea439a20624b8962ff108d23df86
Aj-github-code <ssiyc936@gmail.com>
2024-11-28 13:15:19 +0530

    Create: Test for single and multiple numbers

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,5 +3,14 @@ import add from './calculator';
 describe('Calculator', () => {
     test("If input is string or empty return 0!", () => {
         expect(add('')).toBe(0);
-    })
-})
+    });
+
+    test("If input is single string or number return as it is!", () => {
+        expect(add(1)).toBe(1);
+        expect(add('2')).toBe(2);
+    });
+
+    test("Handle Multiple numbers!", () => {
+        expect(add('1,3,2,5')).toBe(11);
+    });
+});
